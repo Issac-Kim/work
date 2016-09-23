6bc2bd7bafcde52adf8770abba7b447995abcabe
Issac Kim <ikim1@stuy.edu>
2016-09-23 14:50:47 -0400

    work on website

---
+++ occu-table.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from occupations import random_job, get_dict
+
+app = Flask(__name__)
+
+d = get_dict()
+
+
+@app.route("/")
+def welcome():
+    return "Welcome!"
+
+@app.route("/occupations")
+def webpage():
+    job = random_job()
+    return render_template('main.html', keys = d, job = job, percentage = d[job])
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
